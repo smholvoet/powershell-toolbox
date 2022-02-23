ec7a7076fa71e37076faa640dc13156aaff51048
Sander Holvoet <holvoetsander@gmail.com>
2022-02-23 22:20:43 +0100

    Update Get-FilesByExtension.ps1

--- Get-FilesByExtension.ps1
+++ Get-FilesByExtension.ps1
@@ -1,3 +1,9 @@
+# Get all distinct extensions
+(Get-ChildItem -File -Recurse | Select -Property @{N="ExtLow";E={$_.Extension.ToLower()}} -Unique).ExtLow  
+# Save in array
+
+# Loop through array
 Get-ChildItem -Path .\ -Filter *.js -Recurse -File -Name| ForEach-Object {
+    # Add to seperate workbook
     [System.IO.Path]::GetFileNameWithoutExtension($_)
 }
